81f0db5c42cccb56bd4f227186b981e47cee3a3e
liem12313213a <huynhthanhliem732@gmail.com>
2024-08-28 15:18:50 +0700

    Hoan thanh BaiTap02B da fix lai

---
+++ BaiTap02B/BaiTap02B/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTap02B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
