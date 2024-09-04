8c1018f81483f2acf2c68d6803276b325f8c05f0
liem12313213a <huynhthanhliem732@gmail.com>
2024-09-04 14:57:07 +0700

    Hoan thanh BaiTap05

---
+++ BaiTap05/BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,27 @@
+﻿using BaiTap05.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 04";
+            ViewBag.Thang = "Tháng 09";
+            ViewData["Nam"] = "Năm 2024";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh thám"
+            };
+            return View(theloai);
+        }
+        
+    }
+}
