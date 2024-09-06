45b471e9b4d8f71bab12cde3699ecb279d2ef2dc
liem12313213a <huynhthanhliem732@gmail.com>
2024-09-06 14:39:25 +0700

    Hoan Thanh BaiTapController

---
+++ Tuan02Controller/Tuan02Controller/Controllers/Tuan02Controller.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tuan02Controller.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Huỳnh Thanh Liêm";
+            ViewBag.MSSV = "1822031202";
+            ViewData["Nam"] = "Năm 2024";
+            return View();
+        }
+        public ActionResult MayTinh(double a, double b, string pheptinh)
+        {
+            double result;
+            switch (pheptinh)
+            {
+                case "cong":
+                    result = a + b;
+                    break;
+                case "tru":
+                    result = a - b;
+                    break;
+                case "nhan":
+                    result = a * b;
+                    break;
+                case "chia":
+                    if (b == 0) return Content("Không thể chia cho 0!");
+                    result = a / b;
+                    break;
+                default:
+                    return Content("Phép tính không hợp lệ!");
+            }
+            ViewBag.Result = result;
+            return View("MayTinh");
+        }
+        public ActionResult Profile()
+        {
+            return View("Profile");
+        }
+
+    }
+}
