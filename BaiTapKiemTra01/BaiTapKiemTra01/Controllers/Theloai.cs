3f416c31bea230721806f1fbda3e94be9fdef321
liem12313213a <huynhthanhliem732@gmail.com>
2024-09-14 15:01:45 +0700

    Hoan thanh BaiTapKiemTra01

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/Theloai.cs
@@ -0,0 +1,59 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class Theloai : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taiKhoan)
+        {
+            if (taiKhoan != null && taiKhoan.Password != null && taiKhoan.Password.Length > 0)
+            {
+                taiKhoan.Password = taiKhoan.Password + "_da_ma_hoa";
+            }
+            return View(taiKhoan);
+        }
+        public IActionResult BaiTap2()
+        {
+        var sanPhamList = new List<SanPhamViewModel>
+        {
+        new SanPhamViewModel
+        {
+            TenSanPham = "Sản phẩm 1",
+            GiaBan = 100000,
+            Anhmota = "~/wwwrot/img/1.png" 
+        },
+        new SanPhamViewModel
+        {
+            TenSanPham = "Sản phẩm 2",
+            GiaBan = 200000,
+            Anhmota = "~/wwwrot/img/2.png"
+        },
+        new SanPhamViewModel
+        {
+            TenSanPham = "Sản phẩm 3",
+            GiaBan = 300000,
+            Anhmota = "~/wwwrot/images/3.png"
+        },
+        new SanPhamViewModel
+        {
+            TenSanPham = "Sản phẩm 4",
+            GiaBan = 400000,
+            Anhmota = "/wwwrot/img/4.png"
+        },
+        new SanPhamViewModel
+        {
+            TenSanPham = "Sản phẩm 5",
+            GiaBan = 500000,
+            Anhmota = "~/wwwrot/img/5.png"
+        }
+        };
+
+            return View(sanPhamList);
+        }
+    }
+}
---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BaiTapKiemTra01.Models
+{
+    public class TaiKhoanViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string HoTen { get; set; }
+        public int Tuoi { get; set; }
+    }
+    public class SanPhamViewModel
+    {
+        public string TenSanPham { get; set; }
+        public decimal GiaBan { get; set; }
+        public string Anhmota { get; set; } 
+
+    }
+}
